d6864947a29fd22b5e6e566ad2423d39ceab175e
cs416f21 <89888181+cs416f21@users.noreply.github.com>
2021-11-28 20:16:04 -0500

    Update settings.py

--- TodoListWithUserAuthentication/settings.py
+++ TodoListWithUserAuthentication/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ['static']
 
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
